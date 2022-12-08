6ada256199b35953a44e157ae563bb44827e9f60
amidu Qudus <qudusamidu14@gmail.com>
2022-12-08 14:51:46 -0800

    task 4 redone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 /**
  * main - program starting point
  *
- * return: zero  if no error, else non-zero
+ * return: by default returns zero if no error, else non-zero value is returned
  */
 
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
