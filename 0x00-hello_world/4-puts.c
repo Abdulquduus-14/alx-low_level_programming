7092e39c723ccd3dc63e00e88ffbefd9700da79a
amidu Qudus <qudusamidu14@gmail.com>
2022-12-08 14:41:51 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program starting point
+ *
+ * return: zero  if no error, else non-zero
+ */
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
