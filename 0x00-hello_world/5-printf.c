83cea008cff57753f1d7a981ff4eeb790ab612c6
amidu Qudus <qudusamidu14@gmail.com>
2022-12-08 14:59:47 -0800

    printf function use

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: program starting point
+ *
+ * retrun: 0 is returned if no errors, else non-zero value
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
