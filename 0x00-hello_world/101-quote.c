9b38b314cc7b64fc309fdf3695384a4603722496
amidu Qudus <qudusamidu14@gmail.com>
2022-12-08 17:02:57 -0800

    task-8 rechecked

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main:program entry point
+ * main: entering point for program
  *
  * Return: 1 on successful execution
  */
 int main(void)
 {
-	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar,");
+        fprintf(stdout, " 2015-10-19\n");
 	return (1);
 }
 
