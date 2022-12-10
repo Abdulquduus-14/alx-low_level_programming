57a24f423ff536c42b6d67a0fa3782952f0936d8
amidu Qudus <qudusamidu14@gmail.com>
2022-12-09 17:56:55 -0800

    task 9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main: prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 for success
+ * @j: int type
+ */
+
+int main(void)
+{
+	int j;
+	j = 48;
+
+	while (j < 57)
+	{
+		putchar(j);
+		putchar(',');
+		putchar(' ');
+		j++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
