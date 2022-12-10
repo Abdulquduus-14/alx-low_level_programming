0a8f9562797427f30e943e89deba3ee20b16f6cc
amidu Qudus <qudusamidu14@gmail.com>
2022-12-09 17:07:51 -0800

    task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main: prints all single digit numbers of base 10 from 0
+ *
+ * Return: 0 for success
+ * @i: char type
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
