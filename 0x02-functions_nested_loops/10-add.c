a06ed393b726ff16192e04780dfdc6ce43e9b88c
amidu Qudus <qudusamidu14@gmail.com>
2022-12-13 16:24:15 -0800

    task 10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add:  adds two integers and returns the result.
+ *
+ * Return: int type
+ * @b: int parameter
+ * @a: int parameter
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
