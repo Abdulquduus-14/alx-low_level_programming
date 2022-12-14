a5ec1422032e1fe11145140cbf8cfe3b4bddbec1
amidu Qudus <qudusamidu14@gmail.com>
2022-12-13 17:05:26 -0800

    task 11 redone

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	while (n < 98)
 	{
 		if (n == 98)
-			printf("%d ", n);
+			printf("%d", n);
 		else
 			printf("%d, ", n);
 		n++;
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	while (n >= 98)
 	{
 		if (n == 98)
-			printf("%d ", n);
+			printf("%d", n);
 		else
 			printf("%d, ", n);
 		n--;
