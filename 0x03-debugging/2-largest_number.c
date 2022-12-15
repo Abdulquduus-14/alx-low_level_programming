bbe05221584316187a815a9ec75b3222342550a6
amidu Qudus <qudusamidu14@gmail.com>
2022-12-15 09:20:39 -0800

    task 2 debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main: prints the largest of three integers
+ * Return: 0 for success
+ */
+
+/**
+ *
+ *  * largest_number - returns the largest of 3 numbers
+ *
+ *   * @a: first integer
+ *
+ *    * @b: second integer
+ *
+ *     * @c: third integer
+ *
+ *      * Return: largest number
+ *
+ *       */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	largest = a;
+
+	if (b > largest)
+		largest = y;
+	else if (c > largest)
+		largest = c;
+
+	return (largest);
+
+}
