2b6387e284247d3972c773249f7fcc9f8a8a1715
amidu Qudus <qudusamidu14@gmail.com>
2022-12-15 15:15:00 -0800

    task 12 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_number:  prints an integer
+ *
+ * Return: void
+ * @n: int type
+ */
+
+void print_number(int n)
+{
+	int i, j, k, l;
+
+	if (n >= 48 && n < 58)
+		_putchar(n);
+	else if (n > 57 && n < 100)
+	{
+		i = n / 10;
+		j = n % 10;
+		_putchar('0' + i);
+		_putchar('0' + j);
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		k = n / 100;
+		l = n % 100;
+		i = l / 10;
+		j = n % 10;
+		_putchar('0' + k);
+		_putchar(48 + i);
+		_putchar(48 + j);
+	}
+}
