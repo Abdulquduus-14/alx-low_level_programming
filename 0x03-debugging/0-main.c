e2f7b5750ccb56bf5aad09f99bf5249fa9a3e15d
amidu Qudus <qudusamidu14@gmail.com>
2022-12-15 19:07:40 -0800

    task 0 done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main: program starting point
+ *
+ * Return: 0 for success, non=zero otherwise
+ */
+/**
+ * positive_or_negative: prints positive or negative
+ *
+ * Return: void
+ * @i: int type
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 65;
+	positive_or_negative(i);
+
+	return (0);
+}
