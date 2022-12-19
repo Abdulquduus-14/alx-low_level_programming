fcfd6f12a8a35c983612697d44e45578c6b048af
amidu Qudus <qudusamidu14@gmail.com>
2022-12-19 05:21:57 -0800

    task 6 done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2: prints every other character of a string,
+ * starting with the first character, followed by a new
+ *
+ * Return: void
+ * @str: char *
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+
+	_putchar('\0');
+}
