d535e1a3958dab81c9e7674cd76de9467b93a3b7
amidu Qudus <qudusamidu14@gmail.com>
2022-12-19 16:39:09 -0800

    task 9 redone

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	char *s;
+
+	s = dest;
 	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-
+	dest = '\0';
+	dest = s;
 	return (dest);
 }
