860c24bd7b7c88842628b7ea69a4e66b264d8eb8
amidu Qudus <qudusamidu14@gmail.com>
2022-12-21 15:46:06 -0800

    task 6 retried

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,36 @@
-#include "ain.h"
+#include "main.h"
 /**
  * cap_string: capitalizes all words of a string.
- * and returns the new string
+ * @s: string to capitalize
  *
- * return: char * to the new string
- * @s: char * parameter
+ * Return: pointer to new string
  */
-
 char *cap_string(char *s)
 {
 	int i;
 
 	i = 0;
-	while (*s != '\0')
-	{
-		while (!(s[i] >= 97 && s[i] < 123))
-			i++;
 
-		if (s[i - 1] == ',' || s[i - 1] == ';' ||
-		s[i - 1] == '.' ||
-		s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-		s[i - 1] == '(' || s[i - 1] == ')' ||
-		s[i - 1] == '{' || s[i - 1] == '}' ||
-		s[i - 1] == '\t' || s[i - 1] == '\n' ||
-		s[i - 1] == ' ' || i == 0)
-		{
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' ' ||
+		   s[i - 1] == '\t' ||
+		   s[i - 1] == '\n' ||
+		   s[i - 1] == ',' ||
+		   s[i - 1] == ';' ||
+		   s[i - 1] == '.' ||
+		   s[i - 1] == '!' ||
+		   s[i - 1] == '?' ||
+		   s[i - 1] == '"' ||
+		   s[i - 1] == '(' ||
+		   s[i - 1] == ')' ||
+		   s[i - 1] == '{' ||
+		   s[i - 1] == '}' ||
+		   i == 0)
 			s[i] -= 32;
-		}
 		i++;
 	}
-
 	return (s);
 }
