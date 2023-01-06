3a6892fe93403038ba002367e893098f5bce2064
amidu Qudus <qudusamidu14@gmail.com>
2023-01-06 13:25:13 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main: entry point for program execution
+ * prints the number of arguments passed into it.
+ *
+ * Return: 0 for success, otherwise non-zero
+ * @argc: number of arguments
+ * @argv: arrays of pointers to strings
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+
+	exit(EXIT_SUCCESS);
+}
