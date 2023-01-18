67a690ae56056da0aeef5ff031df472566e5d584
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-17 20:16:15 -0800

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog- function that deletes dog memory allocated
+ * @d: pointer to dog 
+ * Return: nothing for success
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+}
