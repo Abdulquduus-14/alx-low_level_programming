8359f1b62a2d0602f869521619534b81fca3fe93
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-21 00:01:47 -0800

    task1 on

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,12 +14,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 
-	for (i = 0; i < n; s = va_arg(p, char), i++)
+	for (i = 0; i < n; *s = va_arg(p, char), i++)
 	{
-		if (*s == NULL)
+		/*if (s == NULL)
 			printf("nil");
 		else
-			printf("%s",s);
+			printf("%s",s);*/
 		if (separator == NULL)
 		{
 		}
