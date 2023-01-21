458207653bada7338533be37345038fec98c537a
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-20 23:04:09 -0800

    task-2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * Return: 0 for success, else non-zero value
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int i;
+	char *s;
+
+	for (i = 0; i < n; s = va_arg(p, char), i++)
+	{
+		if (*s == NULL)
+			printf("nil");
+		else
+			printf("%s",s);
+		if (separator == NULL)
+		{
+		}
+	}
+	va_end(p);
+	printf("\n");
+}
