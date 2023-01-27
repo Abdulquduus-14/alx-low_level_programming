7948b6f10aa5088c6f552c5d5f32123090a2d796
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-26 17:44:59 -0800

    Last task done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void _attribute_((constructor)) hare(void);
+void __attribute__((constructor)) hare(void);
 
 /**
  * hare - prints a string before the main function is executed
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+; file: 101-hello_holberton.asm
+; Desc: @-bit program in assembly that prints Hello,
+; Holberton, followed by a new line.
+
+SECTION .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
