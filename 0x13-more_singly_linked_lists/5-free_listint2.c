fbe8578ff0b3c4e3d21e2351e3c215f0c43f487b
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-30 16:43:56 -0800

    Task -5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 	listint_t *h;
 
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		h = head->next;
+		h = (*head)->next;
 		free(*head);
 		*head = h;
 	}
