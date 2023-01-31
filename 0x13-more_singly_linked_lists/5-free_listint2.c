d343f54f845973fddd4b8329b5c07e0cefc94535
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-30 16:39:11 -0800

    Task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer to beginning of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *h;
+
+
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		h = head->next;
+		free(*head);
+		*head = h;
+	}
+
+	head = NULL;
+}
