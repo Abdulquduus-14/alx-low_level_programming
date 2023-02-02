b1e48b1afab850ec71a5680a5a37da52b57aebfd
Amidu Qudus <qudusamidu14@gmail.com>
2023-02-02 12:28:41 -0800

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number to print its binary equivalent
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	/*unsigned int i, sum;*/
+
+	if (n >= 0 && n < 2)
+		printf("%lu\n", n);
+}
