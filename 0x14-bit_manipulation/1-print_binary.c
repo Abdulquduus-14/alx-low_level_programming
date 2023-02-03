eaf46ebce5c1dc8436c2008a5c5d5290b7abbd25
Amidu Qudus <qudusamidu14@gmail.com>
2023-02-02 17:56:51 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,5 +9,8 @@ void print_binary(unsigned long int n)
 	/*unsigned int i, sum;*/
 
 	/*if (n >= 0 && n < 2)*/
-	printf("%lu\n", n);
+	/*printf("%lu\n", n);*/
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
