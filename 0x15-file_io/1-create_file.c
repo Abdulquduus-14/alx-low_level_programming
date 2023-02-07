bb9e77ae9679f9197184d3ebefa13a6a9705346f
Amidu Qudus <qudusamidu14@gmail.com>
2023-02-06 16:27:40 -0800

    tsk1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file.
+ * @filename: the name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+
+	if (filename == NULL)
+		return (-1);
+	if (filename)
+	{
+		fd = open(filename, O_RDWR | O_TRUNC);
+	}
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_RDWR | O_CREAT);
+		if (fd == -1)
+			return (-1);
+	}
+	return (1);
+}
