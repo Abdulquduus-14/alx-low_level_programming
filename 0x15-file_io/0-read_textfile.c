0336b7f8f0a7bbca788933d713578e2e0d3e6995
Amidu Qudus <qudusamidu14@gmail.com>
2023-02-06 20:55:26 -0800

    Task0 redone

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of file to read from
+ * @letters: the number of letters it should read and print
+ *
+ * Return: number of letters it could read and print for success, else 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int inputfd; /* outputfd;*/
+	/**ssizenumread;*/
+	/*char buf[BUF_SIZE];*/
+
+
+	if (filename == NULL || letters <= 0)
+		return (0);
+
+	inputfd = open(filename, O_RDONLY);
+	if (inputfd == -1)
+		return (0);
+
+	/*while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)*/
+		/*if (write(outputFd, buf, numRead) != numRead)*/
+		/*{*/
+			/*return (0);*/
+		/*}*/
+}
