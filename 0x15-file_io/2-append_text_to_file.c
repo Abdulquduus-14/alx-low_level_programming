be7bc623d575959615466a793449904a4ccf30f3
Amidu Qudus <qudusamidu14@gmail.com>
2023-02-06 16:58:28 -0800

    task2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the file.
+ * @text_content: string to add at the end of the file.
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+	return (1);
+}
