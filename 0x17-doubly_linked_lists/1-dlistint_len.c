fc4978c297065e27f2577e43c6dc9cc890bc9910
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-16 17:01:36 -0700

    tsk1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - find the number of elements in a list
+ * @h: pointer to beginning of list
+ *
+ * Return: the number of elements in a linked dlistint_t list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		length++;
+		current = current->next;
+	}
+	return (length);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a list
+ * @head: pointer to start of list
+ * @n: value of elemnt to add
+ *
+ * Return: the address of the new element on success, else NULL
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *link;
+
+	link = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!link)
+		return (NULL);
+	link->n = n;
+
+	if (head == NULL)
+		*head = link
+	else
+		(*head)->prev = link;
+	return (*head);
+}
