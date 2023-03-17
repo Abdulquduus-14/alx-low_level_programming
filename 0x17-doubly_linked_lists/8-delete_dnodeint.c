7fe615b10c8e79b2d620adda4c85efdc74dd1cb6
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-17 08:04:57 -0700

    On tsk8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	t = *head;
 	if (*head == NULL)
 		return (-1);
-	if (index == 1)
+	if (index == 0)
 	{
 		*head = (*head)->next;
 
@@ -24,19 +24,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			(*head)->prev = NULL;
 		free(t);
 	}
-	while ((k < index) && t->next != NULL)
-	{
-		t = t->next;
-		k++;
-	}
-	if (k != index - 1)
-		return (-1);
-	t2 = t->prev;
-	t2->next = t->next;
-
-	if (t->next)
-		t->next->prev = t2;
-	free(t);
 	return (1);
 }
-
