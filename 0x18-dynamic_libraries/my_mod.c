ed1683a07e8cfbcae8f36e1243213d4fcd87fa4a
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-21 00:20:25 -0700

    CPython

---
+++ 0x18-dynamic_libraries/my_mod.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mod - find the modulus of two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of integers division
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/my_sub.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * sub - sum two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
